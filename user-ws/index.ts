879f61fad59fac141f1a451516b69c8e89479a12
Kush <jangidkush1234@gmail.com>
2025-07-22 23:27:23 +0530

    understanding ws

--- user-ws/index.ts
+++ user-ws/index.ts
@@ -1,6 +1,14 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
+
+// 22:07
+
+interface Room {
+  sockets:WebSocket[]
+}
+
+const rooms: Record<string,Room> = {}
 
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
